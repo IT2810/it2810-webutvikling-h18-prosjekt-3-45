5100f367ea6d2665c8383af28cd4aa063dc96a76
Torjus Iveland <torjusti@gmail.com>
2018-10-04 21:12:57 +0200

    Initial work on sections in todo list

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { finishTodo, deleteTodo, updateTodo } from '../features/todos/actions';
-
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
-
 import Swipeout from 'react-native-swipeout';
+import isBefore from 'date-fns/is_before';
+import isSameDay from 'date-fns/is_same_day';
+import addDays from 'date-fns/add_days';
+import startOfDay from 'date-fns/start_of_day';
 import { SchemaModal } from './todoadder/schemamodal';
 
 // This class is exported both as the default export and as a component
@@ -73,6 +75,26 @@ export class TodoList extends Component {
   render() {
     const openTodos = this.props.todos.filter(todo => !todo.done);
 
+    const sections = ['Someday', 'Overdue', 'Today', 'Tomorrow'];
+
+    const days = [
+      // Todos without any specific set date.
+      openTodos.filter(todo => !todo.date),
+
+      // Todos which are overdue.
+      openTodos.filter(
+        todo => todo.date && isBefore(todo.date, startOfDay(new Date())),
+      ),
+    ];
+
+    for (let i = 0; i < 2; i += 1) {
+      days.push(
+        openTodos.filter(
+          todo => todo.date && isSameDay(addDays(new Date(), i), todo.date),
+        ),
+      );
+    }
+
     return (
       <ScrollView style={styles.container}>
         <View>
@@ -83,44 +105,53 @@ export class TodoList extends Component {
             currentTodo={this.state.pressedTodo}
           />
         </View>
-        <View>
-          {openTodos.map(todo => {
-            const buttons = {
-              left: [
-                {
-                  text: 'Done',
-                  backgroundColor: '#68CC3D',
-                },
-              ],
-              right: [
-                {
-                  text: 'Edit',
-                  onPress: () => this.handlePress(todo.id),
-                  backgroundColor: '#2f95dc',
-                },
-                {
-                  text: 'Delete',
-                  onPress: () => this.handleDelete(todo.id),
-                  backgroundColor: '#bf3b4f',
-                },
-              ],
-            };
 
-            return (
-              <Swipeout
-                left={buttons.left}
-                right={buttons.right}
-                onOpen={(section, row, direction) => {
-                  this.handleOpen(direction, todo.id);
-                }}
-                key={todo.id}
-              >
-                <View style={styles.todo}>
-                  <Text>{todo.text}</Text>
-                </View>
-              </Swipeout>
-            );
-          })}
+        <View>
+          {sections.map((section, i) => (
+            <Fragment key={section}>
+              <View style={styles.todo}>
+                <Text style={styles.sectionTitle}>{section}</Text>
+              </View>
+
+              {days[i].map(todo => {
+                const buttons = {
+                  left: [
+                    {
+                      text: 'Done',
+                      backgroundColor: '#68CC3D',
+                    },
+                  ],
+                  right: [
+                    {
+                      text: 'Edit',
+                      onPress: () => this.handlePress(todo.id),
+                      backgroundColor: '#00AAEE',
+                    },
+                    {
+                      text: 'Delete',
+                      onPress: () => this.handleDelete(todo.id),
+                      backgroundColor: '#bf3b4f',
+                    },
+                  ],
+                };
+
+                return (
+                  <Swipeout
+                    left={buttons.left}
+                    right={buttons.right}
+                    onOpen={(section, row, direction) => {
+                      this.handleOpen(direction, todo.id);
+                    }}
+                    key={todo.id}
+                  >
+                    <View style={styles.todo}>
+                      <Text>{todo.text}</Text>
+                    </View>
+                  </Swipeout>
+                );
+              })}
+            </Fragment>
+          ))}
         </View>
       </ScrollView>
     );
@@ -142,6 +173,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     borderBottomColor: '#CCC',
     borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+
+  sectionTitle: {
+    fontWeight: 'bold',
   },
 });
 
