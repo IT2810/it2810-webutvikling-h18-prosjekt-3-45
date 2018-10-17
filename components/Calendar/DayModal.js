7b0d147a713c1d70008395752608a5de748d62e6
Torjus Iveland <torjusti@gmail.com>
2018-10-17 12:02:49 +0200

    Fix toasts not appearing in calendar modal

--- components/Calendar/DayModal.js
+++ components/Calendar/DayModal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Modal, View } from 'react-native';
-import { Text } from 'native-base';
+import { Text, Root } from 'native-base';
 import format from 'date-fns/format';
 import {
   Container,
@@ -22,37 +22,39 @@ class DayModal extends Component {
   render() {
     return (
       <Modal visible={this.props.visible} onRequestClose={this.props.onClose}>
-        <Container>
-          <Header>
-            <Left>
-              <Button transparent onPress={this.props.onClose}>
-                <Icon ios="ios-arrow-back" android="md-arrow-back" />
-              </Button>
-            </Left>
+        <Root>
+          <Container>
+            <Header>
+              <Left>
+                <Button transparent onPress={this.props.onClose}>
+                  <Icon ios="ios-arrow-back" android="md-arrow-back" />
+                </Button>
+              </Left>
 
-            <Body>
-              <Title>{format(this.props.day, 'DD MMM YYYY')}</Title>
-            </Body>
+              <Body>
+                <Title>{format(this.props.day, 'DD MMM YYYY')}</Title>
+              </Body>
 
-            <Right />
-          </Header>
+              <Right />
+            </Header>
 
-          <Content>
-            <View style={[todoStyles.todo, tasksStyles.section]}>
-              <Text style={tasksStyles.sectionTitle}>Unfinished</Text>
-            </View>
+            <Content>
+              <View style={[todoStyles.todo, tasksStyles.section]}>
+                <Text style={tasksStyles.sectionTitle}>Unfinished</Text>
+              </View>
 
-            <TodoList todos={this.props.todos.filter(todo => !todo.done)} />
+              <TodoList todos={this.props.todos.filter(todo => !todo.done)} />
 
-            <View style={[todoStyles.todo, tasksStyles.section]}>
-              <Text style={tasksStyles.sectionTitle}>Finished</Text>
-            </View>
+              <View style={[todoStyles.todo, tasksStyles.section]}>
+                <Text style={tasksStyles.sectionTitle}>Finished</Text>
+              </View>
 
-            <TodoList todos={this.props.todos.filter(todo => todo.done)} />
-          </Content>
-        </Container>
+              <TodoList todos={this.props.todos.filter(todo => todo.done)} />
+            </Content>
+          </Container>
 
-        <TodoAdder initialDate={this.props.day} />
+          <TodoAdder initialDate={this.props.day} />
+        </Root>
       </Modal>
     );
   }
